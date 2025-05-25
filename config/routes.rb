1baf27c749175c88a03e5985be10591af18578ad
baileydodds <ddodds@chicagobooth.edu>
2025-05-25 20:30:15 +0000

    FINAL

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   get("/", controller: "users", action: "index")
   get("/users", controller: "users", action: "index")
+
   get("/users/:username", controller: "users", action: "show")
   post("/add_user", controller: "users", action: "create")
   post("/update_user/:user_id", controller: "users", action: "update")
+
   get("/photos", controller: "photos", action: "index")
   get("/photos/:photo_id", controller: "photos", action: "show")
   get("/delete_photo/:photo_id", controller: "photos", action: "delete")
